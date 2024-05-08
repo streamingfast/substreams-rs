f4f960cc4cf94e9a120a63261fef767563c49a5f
arnaudberger <arnaudberger84@gmail.com>
2024-05-08 09:28:03 -0400

    Add index keys within pb

--- substreams/src/pb/mod.rs
+++ substreams/src/pb/mod.rs
@@ -1,2 +1,15 @@
+// @generated
+pub mod sf {
+    pub mod substreams {
+        pub mod index {
+            // @@protoc_insertion_point(attribute:sf.substreams.index.v1)
+            pub mod v1 {
+                include!("sf.substreams.index.v1.rs");
+                // @@protoc_insertion_point(sf.substreams.index.v1)
+            }
+        }
+    }
+}
+
 #[path = "./sf.substreams.v1.rs"]
 pub mod substreams;
